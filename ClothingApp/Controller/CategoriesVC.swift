ee80bcdc21887a5145c04c367bc28077df6270c8
Travis Bowen <Travisbowen23@gmail.com>
2018-07-09 20:34:24 -0700

    Added assets and created categories UI

---
+++ ClothingApp/Controller/CategoriesVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClothingApp
+//
+//  Created by Travis Bowen on 7/9/18.
+//  Copyright © 2018 UpscaleApps. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
---
+++ ClothingApp/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  ClothingApp
+//
+//  Created by Travis Bowen on 7/9/18.
+//  Copyright © 2018 UpscaleApps. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
