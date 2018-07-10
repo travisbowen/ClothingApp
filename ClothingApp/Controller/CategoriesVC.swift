3ef5462565a4d8927f186384ae624b8b4f8b4332
Travis Bowen <Travisbowen23@gmail.com>
2018-07-09 21:52:51 -0700

    Finished CategoryVC tableview

--- ClothingApp/Controller/CategoriesVC.swift
+++ ClothingApp/Controller/CategoriesVC.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class CategoriesVC: UIViewController {
+class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell{
+            let categories = DataService.instance.getCategories()
+            let category = categories[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
     }
 }
 
--- ClothingApp/View/CategoryCell.swift
+++ ClothingApp/View/CategoryCell.swift
@@ -18,10 +18,8 @@ class CategoryCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func updateViews(category: Category){
+        categoryImage.image = UIImage(named: category.image)
+        categoryLabel.text = category.title
     }
-
 }
